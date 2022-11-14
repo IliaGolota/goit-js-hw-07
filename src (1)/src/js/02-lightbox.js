a59882be9804d44474763206465d163175da0cf3
IliaGolota <iliagolot@gmail.com>
2022-11-14 11:06:26 +0200

    додає друге завдання

--- src (1)/src/js/02-lightbox.js
+++ src (1)/src/js/02-lightbox.js
@@ -1,24 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const galleryEl = document.querySelector('ul.gallery');
+function makeGallery(arr) {
+  const galleryItemsMarkup = arr
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}" /></a></li>`;
+    })
+    .join('');
+  return galleryEl.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+}
+makeGallery(galleryItems);
+const gallery = new SimpleLightbox('ul.gallery a');
 console.log(galleryItems);
-const listEl = document.querySelector('.gallery');
-
-const listTemplate = (preview, original, description) => {
-  return `<li><a class="gallery__item" href="${original}">
-<img class="gallery__image" src="${preview}" alt="${description}" />
-</a></li>`;
-};
-
-const render = () => {
-  const arrayFromListTemplate = galleryItems.map((item) =>
-    listTemplate(item.preview, item.original, item.description)
-  );
-  listEl.innerHTML = arrayFromListTemplate.join('');
-};
-render();
-
-const galleryEl = new simpleLightbox('ul.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
